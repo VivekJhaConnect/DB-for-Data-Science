1196ab61d4644123157c52a410d218b20bfcd4a0
VivekJhaConnect <vivekjhahsp@gmail.com>
2025-04-09 23:27:03 +0530

    Week2-2025 SQL WHERE condition Learning

--- SQL-PostgresDB/2_basic-filtering/where_condition.sql
+++ SQL-PostgresDB/2_basic-filtering/where_condition.sql
@@ -12,12 +12,30 @@ What is the last name of our customer with first name 'ERICA'
 
 */
 
-SELECT COUNT(*) FROM public.payment WHERE customer_id = 100;
+-- SELECT COUNT(*) FROM public.payment WHERE customer_id = 100;
 
-SELECT last_name FROM public.customer WHERE first_name = 'ERICA';
+-- SELECT last_name FROM public.customer WHERE first_name = 'ERICA';
+
+/* Challenges:
+
+The inventory manager asks you how rentals have not been returned yet (return_date is null)
+
+The sales manager asks you how for a list of all the payment_ids with an amount less than or equal to $2.
+Include payment_id and the amount
+
+*/
+
+-- SELECT COUNT(*) FROM public.rental WHERE return_date IS NULL;
+
+-- SELECT payment_id, amount FROM public.payment WHERE amount <= 2;
 
 
+/*
 
+WHERE with AND/OR
 
+*/
 
+SELECT * FROM public.payment WHERE customer_id = 100 OR customer_id = 101;
 
+SELECT * FROM public.payment WHERE customer_id = 100 AND amount > 2;
